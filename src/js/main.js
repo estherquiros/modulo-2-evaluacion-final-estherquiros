6b603c78c7fb896a338a98cd25d60f45003d3405
Esther Quirós <estherbequiros@gmail.com>
2025-11-12 11:06:46 +0100

    styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,10 @@
 "use strict";
 const API_URL = "https://fakestoreapi.com/products";
 const productsList = document.querySelector(".js_products-list");
+const shoppingCartList = document.querySelector(".js_shopping-cart");
 
 let products = [];
+let shoppingCart = [];
 
 function fetchProducts() {
   fetch(API_URL)
@@ -18,11 +20,15 @@ function renderProducts(products) {
   let html = "";
 
   for (const product of products) {
-    html += `<li>"
-    <img src="${product.image}"/>
-    <h2>${product.title}</h2>
-    <p>${product.price}</p>
-    <button data-id="${product.id}">Buy</button>
+    html += `<li class="product-card">
+    <div class="product-img-title"> 
+    <img class="product-img"src="${product.image}"/>
+    <h2 class="product-title">${product.title}</h2>
+    </div>
+    <div class="product-price-button">
+    <p class="product-price">${product.price}</p>
+    <button class="product-button" data-id="${product.id}">Buy</button>
+    </div>
     </li>`;
   }
 
@@ -34,4 +40,21 @@ fetchProducts();
 productsList.addEventListener("click", (ev) => {
   ev.preventDefault();
   console.log(ev.target.dataset.id);
+  const productId = ev.target.dataset.id;
+  const product = products.find((prd) => {
+    return Number(prd.id) === Number(productId);
+  });
+
+  console.log(product);
+
+  const existProductOnShoppingCart = shoppingCart.findIndex((prd) => {
+    return Number(prd.id) === Number(productId);
+  });
+  console.log(existProductOnShoppingCart);
+
+  if (!Boolean(existProductOnShoppingCart)) {
+    shoppingCart.push(product);
+  }
+
+  console.log(shoppingCart);
 });
